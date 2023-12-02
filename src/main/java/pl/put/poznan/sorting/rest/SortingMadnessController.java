37255d58bd13d717736a89eea3c2bc027c8d6a05
Dawid <dawid.drozdzynski.g4@gmail.com>
2023-12-02 12:09:16 +0100

    REST configuration and strategy design pattern implementation

--- src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
@@ -2,45 +2,33 @@ package pl.put.poznan.sorting.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.sorting.logic.SortingMadness;
+import pl.put.poznan.sorting.logic.SortingMadnessLogic;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 
 @RestController
-@RequestMapping("/{text}")
 public class SortingMadnessController {
 
     private static final Logger logger = LoggerFactory.getLogger(SortingMadnessController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+    @RequestMapping(value = "/sortInts", method = RequestMethod.POST, produces = "application/json")
+    public ArrayList<Integer> sortInts(@RequestParam ArrayList<Integer> arr,
+                                       @RequestParam String algo) throws Exception {
+        logger.info("Given array: " + arr.toString());
+        logger.info("Chosen algorithm: " + algo);
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        SortingMadness transformer = new SortingMadness(transforms);
-        return transformer.transform(text);
+        SortingMadnessLogic sorting = new SortingMadnessLogic(algo);
+        return sorting.sort(arr);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+    @RequestMapping(value = "/sortStrings", method = RequestMethod.POST, produces = "application/json")
+    public ArrayList<String> sortStrings(@RequestParam ArrayList<String> arr,
+                                         @RequestParam String algo) throws Exception {
+        logger.info("Given array: " + arr.toString());
+        logger.info("Chosen algorithm: " + algo);
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        SortingMadness transformer = new SortingMadness(transforms);
-        return transformer.transform(text);
+        SortingMadnessLogic sorting = new SortingMadnessLogic(algo);
+        return sorting.sort(arr);
     }
-
-
-
 }
-
-
